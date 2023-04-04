1d0acfceb37edd8bc6180076e148e28097f8fe2d
whoto1 <118448487+whoto1@users.noreply.github.com>
2023-04-04 12:45:12 +0700

    Update nuss.cpp

--- nuss.cpp
+++ nuss.cpp
@@ -1,17 +1,24 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <sstream>
+#include <string>
 
-int main()
+int main(int argc, char* argv[])
 {
-    int x,n,m;
-    m = 640;
-    n = 480;
-    bool k = false;
-    std::vector<int>mass;
-    mass.clear();
+    int x, n, m;
+    float y;
+    
+    std::stringstream str(argv[1]);
+    std::stringstream str1(argv[2]);
+    str >> m;
+    str1 >> n;
+    std::cout << m << std::endl;
+    std::cout << n << std::endl;
+    bool k = false; // чтобы считать нужную информацию
+    std::vector<float>arr;
     std::ifstream in("IR004872.bmt", std::ios::binary);
     while (in.read((char*)&x, sizeof(int)))
     {
@@ -25,18 +32,18 @@ int main()
                     for (int i = 0;i < n * m;i++)
                     {
 
-                        in.read((char*)&x, sizeof(int));
-                        mass.push_back(x);
+                        in.read((char*)&y, sizeof(float));
+                        arr.push_back(y);
                     }
                     break;
                 }
                 k = true;
             }
         }
-        
+
     }
     in.close();
     for (int i = 0;i < 10;i++)
-        std::cout << mass[i]<<std::endl;
+        std::cout << arr[i] << std::endl;
     return 0;
 }
