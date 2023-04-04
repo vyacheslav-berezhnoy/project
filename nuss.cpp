0953724862ef801226a44525bf3c2821cce75d26
vyacheslav-berezhnoy <118507194+vyacheslav-berezhnoy@users.noreply.github.com>
2023-04-04 11:39:37 +0700

    Add files via upload

---
+++ nuss.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+
+int main()
+{
+    int x,n,m;
+    m = 640;
+    n = 480;
+    bool k = false;
+    std::vector<int>mass;
+    mass.clear();
+    std::ifstream in("IR004872.bmt", std::ios::binary);
+    while (in.read((char*)&x, sizeof(int)))
+    {
+        if (x == m)
+        {
+            in.read((char*)&x, sizeof(int));
+            if (x == n)
+            {
+                if (k)
+                {
+                    for (int i = 0;i < n * m;i++)
+                    {
+
+                        in.read((char*)&x, sizeof(int));
+                        mass.push_back(x);
+                    }
+                    break;
+                }
+                k = true;
+            }
+        }
+        
+    }
+    in.close();
+    for (int i = 0;i < 10;i++)
+        std::cout << mass[i]<<std::endl;
+    return 0;
+}
